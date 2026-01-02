e8868e3bbf4f4fdbb03a42c884b60994b189239b
cesar <cesarbarraza13@gmail.com>
2026-01-01 19:56:30 -0600

    added shorter commands

--- main.py
+++ main.py
@@ -6,30 +6,27 @@ import help_rdpm
 
 def main():
     print("---------------------------------------------------")
-    print("Pi Remote Device Power Management Tool v2025.12.13")
+    print("Pi Remote Device Power Management Tool v2026.01.01")
     print("---------------------------------------------------")
     print("Enter Command")
     usr_input = input(":~ $ ").lower().strip()
-    if usr_input == "on":
+    if usr_input == "on" or usr_input == "off" or usr_input == "o":
         pwr_ctrl.on_off()
         main()
-    elif usr_input == "off":
-        pwr_ctrl.on_off()
-        main()
-    elif usr_input == "reset":
+    elif usr_input == "reset" or usr_input == "r":
         reset.reset()
         main()
-    elif usr_input == "status":
+    elif usr_input == "status" or usr_input == "s":
         status.main()
         main()
-    elif usr_input == "help":
+    elif usr_input == "help" or usr_input == "h":
         help_rdpm.help()
         main()
-    elif usr_input == "exit":
+    elif usr_input == "exit" or usr_input == "e":
         input("Press the 'Enter' key to exit...")
         exit
     else:
-        print("Command not supported. Type 'help' for list of commands.")
+        print("Command not supported. Type 'h(elp)' for list of commands.")
         print("")
         main()
 main()

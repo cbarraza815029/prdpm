1cefd139f6f621323b06bd86f7705b5e80b884c7
cesar <cesarbarraza13@gmail.com>
2026-06-15 20:33:55 -0500

    added feature: change pins in program

--- prdpm.py
+++ prdpm.py
@@ -2,8 +2,10 @@ import os
 import platform
 import subprocess
 from pathlib import Path
+from gpiozero import DigitalOutputDevice
+from gpiozero.pins.rpigpio import RPiGPIOPin
 from time import sleep
-import vars
+import json
 
 #---------------------------------------------------------------------------------------------------
 #Program Title Function
@@ -13,7 +15,7 @@ def program_title():
     #clear_scren = "cls" if platform.system().lower() == "windows" else "clear"
     #os.system(clear_scren)
     print("---------------------------------------------------")
-    print("Pi Remote Device Power Management Tool v2026.05.16")
+    print("Pi Remote Device Power Management Tool v2026.06.15")
     print("---------------------------------------------------")
 
 #---------------------------------------------------------------------------------------------------
@@ -67,14 +69,14 @@ def ping(host):
 #---------------------------------------------------------------------------------------------------
 #Uses Ping function to check whether target device is reachable. Reachable = On while 
 #Unreachable = Off or device still booting up (if previously powered/ reset)
-def status():
+def status(target_host_param):
     program_title()
     print("Checking device status...")
-    if ping(vars.target_host) == True:
-        print(f"Host {vars.target_host} is on")
+    if ping(target_host_param) == True:
+        print(f"Host {target_host_param} is on")
         print("")
     else:
-        print(f"Host {vars.target_host} is off")
+        print(f"Host {target_host_param} is off")
         print("")
 
 #---------------------------------------------------------------------------------------------------
@@ -83,14 +85,16 @@ def status():
 #Submenu that uses the Ping function to check whether target device is reachable. If it is, 
 #the On/Off function prompts the user if they would like to shutdown the device. If it is not
 #reachable, the function asks if you would like to power on the device.
-def on_off():
+def on_off(target_host_param, dict_param):
+    pwr = DigitalOutputDevice(dict_param["pwr"])
+
     program_title()
-    if ping(vars.target_host) == True:
-        print(f"{vars.target_host} is on")
-        usr_input = input(f"Shutdown {vars.target_host} (y/n)?: ").lower().strip()
+    if ping(target_host_param) == True:
+        print(f"{target_host_param} is on")
+        usr_input = input(f"Shutdown {target_host_param} (y/n)?: ").lower().strip()
         if usr_input == "y" or usr_input == "yes":
-            print(f"Shutting down {vars.target_host}...")
-            push_button(vars.pwr)
+            print(f"Shutting down {target_host_param}...")
+            push_button(pwr)
             print("")
         elif usr_input == "n" or usr_input == "no":
             print("Shutdown canceled")
@@ -99,11 +103,11 @@ def on_off():
             print("Command not supported")
             print("")
     else:
-        print(f"{vars.target_host} is off")
-        usr_input = input(f"Power on {vars.target_host} (y/n)?: ").lower().strip()
+        print(f"{target_host_param} is off")
+        usr_input = input(f"Power on {target_host_param} (y/n)?: ").lower().strip()
         if usr_input == "y" or usr_input == "yes":
-            print(f"Powering on {vars.target_host}...")
-            push_button(vars.pwr)
+            print(f"Powering on {target_host_param}...")
+            push_button(pwr)
             print("")
         elif usr_input == "n" or usr_input == "no":
             print("Power on canceled")
@@ -118,14 +122,16 @@ def on_off():
 #Submenu that uses the Ping function to check whether target device is reachable. If it is, 
 #the Reset function prompts the user if they would like to reset the device. If it is 
 #not reachable, the function returns to the manin menu.
-def reset():
+def reset(target_host_param, dict_param):
+    rst = DigitalOutputDevice(dict_param["pwr_rst"])
+
     program_title()
-    if ping(vars.target_host):
-        print(f"{vars.target_host} is on")
-        usr_input = input(f"Reset {vars.target_host} (y/n)?: ").lower().strip()
+    if ping(target_host_param) == True:
+        print(f"{target_host_param} is on")
+        usr_input = input(f"Reset {target_host_param} (y/n)?: ").lower().strip()
         if usr_input == "y" or usr_input == "yes":
-            print(f"Resetting {vars.target_host}...")
-            push_button(vars.pwr_rst)
+            print(f"Resetting {target_host_param}...")
+            push_button(rst)
             print("")
         elif usr_input == "n" or usr_input == "no":
             print("Reset canceled")
@@ -134,7 +140,7 @@ def reset():
             print("Command not supported")
             print("")
     else:
-        print(f"{vars.target_host} is off")
+        print(f"{target_host_param} is off")
         print("")
 
 #---------------------------------------------------------------------------------------------------
@@ -144,14 +150,34 @@ def reset():
 #and usr_input var as parameters. Then opens vars.py and creates a list from its contents. The list 
 #gets indexed and looks for the line of code containing "target_host" after which it gets changed 
 #to the usr_input var. Completes the operation by writing the list back to vars.py.
-def change_vars(file_param, new_target_host):
-    with open(file_param, "r") as file:
-        file_lines = file.readlines()
-        for index, line in enumerate(file_lines, 1):
-            if "target_host" in line:
-                file_lines[index - 1] = f"{'target_host = "'}{new_target_host}{'"\n'}"
-    with open(file_param, "w") as file:
-        file.writelines(file_lines)
+def change_vars(file_param, dict_param, usr_input_ip_or_pin, usr_input_param):
+    if type(usr_input_param) is str:
+        dict_param["target_host"] = usr_input_param
+    if type(usr_input_param) is int:
+        if usr_input_ip_or_pin == "power" or usr_input_ip_or_pin == "pwr" or usr_input_ip_or_pin == "p":
+            dict_param["pwr"] = usr_input_param
+        if usr_input_ip_or_pin == "reset" or usr_input_ip_or_pin == "rst" or usr_input_ip_or_pin == "r":
+            dict_param["pwr_rst"] = usr_input_param
+    with open(file_param, 'w', encoding='utf-8') as file:
+            json.dump(dict_param, file, ensure_ascii=False)
+    return dict_param
+
+#---------------------------------------------------------------------------------------------------
+#Check ValueError Function
+#---------------------------------------------------------------------------------------------------
+#Checks for value errors in user input when changing power or reset pins
+def check_value_error(pin_pwr_or_rst):
+    if pin_pwr_or_rst == "power" or pin_pwr_or_rst == "pwr" or pin_pwr_or_rst == "p":
+        pin_type = "power"
+    if pin_pwr_or_rst == "reset" or pin_pwr_or_rst == "rst" or pin_pwr_or_rst == "r":
+        pin_type = "reset"
+    pin_final_val = None
+    while pin_final_val is None:
+        try:
+            pin_final_val = int(input(f"New {pin_type} pin: ").lower().strip())
+        except ValueError:
+            print("Integer values only. Try again!")
+    return pin_final_val
 
 #---------------------------------------------------------------------------------------------------
 #Change Sub Menu Function
@@ -162,41 +188,78 @@ def change_vars(file_param, new_target_host):
 #run the change_vars function that changes the target_host variable in the vars.py file. It 
 #completes by printing a success message and changes the target_host var in memory so you don't 
 #need to restart prdpm for the ip/hostname change to take effect.
-def change_vars_sub_menu():
+def change_vars_sub_menu(file_path_param, dict_param_change_menu, target_host_param):
     program_title()
-    file = "vars.py"
-    file_dir = Path(__file__).parent.resolve()
-    dir_slash = "\\" if platform.system().lower() == "windows" else "/"
-    file_abs_path = f"{file_dir}{dir_slash}{file}"
-    
-    print(f"{'Current IP/ hostname: '}{vars.target_host}")
-    usr_input = input("New IP/ hostname: ").strip()
-    if usr_input == "exit" or usr_input == "e":
-        print("Returning to main menu...")
-        print("")
-    else:
-        change_vars(file_abs_path, usr_input)
-        print(f"{'Success! IP address/ hostname changed from '}{'"'}{vars.target_host}{'"'}{' to '}{'"'}{usr_input}{'"'}")
-        vars.target_host = usr_input
-        print("")
+    pwr = dict_param_change_menu["pwr"]
+    pwr_rst = dict_param_change_menu["pwr_rst"]
+    while True:
+        usr_input_init = input("Change IP/ hostname or pins?: ").strip().lower()
+        if usr_input_init == "ip" or usr_input_init == "i" or usr_input_init == "hostname" or usr_input_init == "host" or usr_input_init == "name" or usr_input_init == "h":
+            print(f"{'Current IP/ hostname: '}{target_host_param}")
+            usr_input_ip = input("New IP/ hostname: ").strip()
+            sett_dict = change_vars(file_path_param, dict_param_change_menu, usr_input_init, usr_input_ip)
+            #print(f"{'Success! IP address/ hostname changed from '}{'"'}{target_host}{'"'}{' to '}{'"'}{usr_input_ip}{'"'}")
+            return sett_dict
+        if usr_input_init == "pins" or usr_input_init == "pin" or usr_input_init == "p":
+            usr_input_pin_sel = input("Change power or reset pin?: ").strip().lower()
+            if usr_input_pin_sel == "power" or usr_input_pin_sel == "pwr" or usr_input_pin_sel == "p":
+                print(f"{'Current power pin: '}{pwr}")
+                usr_input_pin_pwr = check_value_error(usr_input_pin_sel)
+                sett_dict = change_vars(file_path_param, dict_param_change_menu, usr_input_pin_sel, usr_input_pin_pwr)
+                #print(f"{'Success! Power pin changed from '}{'"'}{pwr}{'"'}{' to '}{'"'}{usr_input_ip}{'"'}")
+                return sett_dict
+            if usr_input_pin_sel == "reset" or usr_input_pin_sel == "rst" or usr_input_pin_sel == "r":
+                print(f"{'Current reset pin: '}{pwr_rst}")
+                usr_input_pin_rst = check_value_error(usr_input_pin_sel)
+                sett_dict = change_vars(file_path_param, dict_param_change_menu, usr_input_pin_sel, usr_input_pin_rst)
+                return sett_dict
+        if usr_input_init == "exit" or usr_input_init == "e" or usr_input_init == "back" or usr_input_init == "b" or usr_input_init == "return" or usr_input_init == "rtrn":
+            print("Returning to main menu...")
+            print("")
+            break
+        else:
+            print("Command not supported. Type 'h(elp)' for list of commands.")
+            print("")
 
 #---------------------------------------------------------------------------------------------------
 #Main Menu Function
 #---------------------------------------------------------------------------------------------------
 #Main menu of prdpm.
 def main():
+    file = "settings.json"
+    file_dir = Path(__file__).parent.resolve()
+    dir_slash = "\\" if platform.system().lower() == "windows" else "/"
+    file_abs_path = f"{file_dir}{dir_slash}{file}"
+
+    if Path(file_abs_path).is_file():
+        pass
+    else:
+        Path(file_abs_path).touch(exist_ok=True)
+        default_settings = {
+            "pwr": 23,
+            "pwr_rst": 27,
+            "target_host": "freenas.local"
+        }
+        with open(file_abs_path, 'w', encoding='utf-8') as file:
+            json.dump(default_settings, file, ensure_ascii=False)
+
+    with open(file_abs_path, 'r', encoding='utf-8') as settings_read_file:
+        settings_dict = json.load(settings_read_file)
+    
     while True:
+        target_host = settings_dict["target_host"]
+        
         program_title()
         usr_input = input("Command: ").lower().strip()
         print("")
         if usr_input == "on" or usr_input == "off" or usr_input == "o":
-            on_off()
+            on_off(target_host, settings_dict)
         elif usr_input == "reset" or usr_input == "r":
-            reset()
+            reset(target_host, settings_dict)
         elif usr_input == "status" or usr_input == "s":
-            status()
+            status(target_host)
         elif usr_input == "change" or usr_input == "c":
-            change_vars_sub_menu()
+            settings_dict = change_vars_sub_menu(file_abs_path, settings_dict, target_host)
         elif usr_input == "help" or usr_input == "h":
             help()
         elif usr_input == "exit" or usr_input == "e":
@@ -205,5 +268,4 @@ def main():
         else:
             print("Command not supported. Type 'h(elp)' for list of commands.")
             print("")
-
 main()
